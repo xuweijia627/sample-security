785d7d10d4b25d7ec3f796bcba61536e10e4aff9
xuweijia <xuweijiajia@126.com>
2019-04-02 23:13:30 +0800

    password

--- sample-security-browser/src/main/java/com/sample/security/browser/MyUserDetailsService.java
+++ sample-security-browser/src/main/java/com/sample/security/browser/MyUserDetailsService.java
@@ -1,19 +1,26 @@
 package com.sample.security.browser;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class MyUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         log.info("username={}",username);
-        return new User(username, "123456", AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
+        String password = passwordEncoder.encode("123456");
+        log.info("password={}",password);
+        return new User(username, password, AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
     }
 }
