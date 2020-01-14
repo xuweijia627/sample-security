440994f879ba3189ce2b53331bc763b92a74357b
xuweijia <xuweijiajia@126.com>
2020-01-14 18:03:26 +0800

    图形验证码重构

--- sample-security-core/src/main/java/com/sample/security/core/validate/code/ValidateCodeController.java
+++ sample-security-core/src/main/java/com/sample/security/core/validate/code/ValidateCodeController.java
@@ -1,10 +1,8 @@
 package com.sample.security.core.validate.code;
 
-import com.sample.security.core.properties.SecurityProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.connect.web.HttpSessionSessionStrategy;
 import org.springframework.social.connect.web.SessionStrategy;
-import org.springframework.web.bind.ServletRequestUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.ServletWebRequest;
@@ -12,10 +10,7 @@ import org.springframework.web.context.request.ServletWebRequest;
 import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.util.Random;
 
 /**
  * @author xuWeiJia
