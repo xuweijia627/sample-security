85a3e090986fe06dea93d047810a31d0b78e4dcd
xuweijia <xuweijiajia@126.com>
2020-03-02 21:50:00 +0800

    绑定与解绑微信

---
+++ sample-security-core/src/main/java/com/sample/security/core/social/ImoocConnectView.java
@@ -0,0 +1,24 @@
+package com.sample.security.core.social;
+
+import org.springframework.web.servlet.view.AbstractView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+/**
+ * @author xuWeiJia
+ * @date 2020/03/02
+ */
+public class ImoocConnectView extends AbstractView {
+    @Override
+    protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request,
+                                           HttpServletResponse response) throws Exception {
+        response.setContentType("text/html;charset=UTF-8");
+        if (model.get("connections") == null) {
+            response.getWriter().write("<h3>解绑成功</h3>");
+        } else {
+            response.getWriter().write("<h3>绑定成功</h3>");
+        }
+    }
+}
--- sample-security-core/src/main/java/com/sample/security/core/social/weixin/config/WeixinAutoConfiguration.java
+++ sample-security-core/src/main/java/com/sample/security/core/social/weixin/config/WeixinAutoConfiguration.java
@@ -5,12 +5,16 @@ package com.sample.security.core.social.weixin.config;
 
 import com.sample.security.core.properties.SecurityProperties;
 import com.sample.security.core.properties.WeixinProperties;
+import com.sample.security.core.social.ImoocConnectView;
 import com.sample.security.core.social.weixin.connect.WeixinConnectionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.social.SocialAutoConfigurerAdapter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.social.connect.ConnectionFactory;
+import org.springframework.web.servlet.View;
 
 /**
  * 微信登录配置
@@ -37,6 +41,12 @@ public class WeixinAutoConfiguration extends SocialAutoConfigurerAdapter {
 		WeixinProperties weixinConfig = securityProperties.getSocial().getWeixin();
 		return new WeixinConnectionFactory(weixinConfig.getProviderId(), weixinConfig.getAppId(),
 				weixinConfig.getAppSecret());
+	}
+
+	@Bean({"connect/weixinConnect", "connect/weixinConnected"})
+	@ConditionalOnMissingBean(name = "weixinConnectedView")
+	public View weixinConnectedView() {
+		return new ImoocConnectView();
 	}
 
 }
