659730f4410a0696a583c6f83cee27e1d2579d7c
xuweijia <xuweijiajia@126.com>
2020-03-19 00:33:17 +0800

    用户名密码登录成功后返回token

--- sample-security-app/src/main/java/com/sample/security/app/ImoocResourceServerConfig.java
+++ sample-security-app/src/main/java/com/sample/security/app/ImoocResourceServerConfig.java
@@ -1,7 +1,16 @@
 package com.sample.security.app;
 
+import com.sample.security.core.authentication.mobile.SmsCodeAuthenticationSecurityConfig;
+import com.sample.security.core.properties.SecurityConstants;
+import com.sample.security.core.properties.SecurityProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.social.security.SpringSocialConfigurer;
 
 /**
  * @author xuWeiJia
@@ -9,5 +18,43 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
  */
 @Configuration
 @EnableResourceServer
-public class ImoocResourceServerConfig {
+public class ImoocResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationSuccessHandler sampleAuthenticationSuccessHandler;
+    @Autowired
+    private AuthenticationFailureHandler sampleAuthenticationFailureHandler;
+    @Autowired
+    private SmsCodeAuthenticationSecurityConfig smsCodeAuthenticationSecurityConfig;
+    @Autowired
+    private SpringSocialConfigurer imoocSocialSecurityConfig;
+    @Autowired
+    private SecurityProperties securityProperties;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        http.formLogin()
+                //.loginPage(SecurityConstants.DEFAULT_LOGIN_PAGE_URL)
+                .loginProcessingUrl(SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_FORM)
+                .successHandler(sampleAuthenticationSuccessHandler)
+                .failureHandler(sampleAuthenticationFailureHandler);
+
+        http.apply(smsCodeAuthenticationSecurityConfig)
+                .and()
+                .apply(imoocSocialSecurityConfig)
+                .and()
+                .authorizeRequests()
+                .antMatchers(SecurityConstants.DEFAULT_UNAUTHENTICATION_URL,
+                        securityProperties.getBrowser().getLoginPage(),
+                        SecurityConstants.DEFAULT_VALIDATE_CODE_URL_PREFIX + "/*",
+                        securityProperties.getBrowser().getSignUpUrl(),
+                        securityProperties.getBrowser().getSession().getSessionInvalidUrl(),
+                        "/user/regist")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .csrf().disable(); // and().csrf().disable() 关闭跨站请求伪造防护
+    }
 }
